8ade0a09fdec21ab8b610ef79344652b93b6bc70
JesperKYH <jesper.pettersson@student.kyh.se>
2021-02-14 10:25:28 +0100

    Done with tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,51 @@
 // Detta är en html template, man kan säga att det är html för att göra det lätt
 // Varje component måste ha en template tag
 <template>
-  <div id="wrapper">
+  <!-- <div id="wrapper"> -->
     <h1>{{title}}</h1>
-    <p id="desc">{{desc}}</p>
 
     <input type="text" ref="name"/>
     <button @click="handleClick">Click Me</button>
-</div>
+    <teleport to=".modals" v-if="showModal">
+      <!-- Inne i Modal så är det något som kallas ett slot, vi fixar template som props ungefär -->
+      <Modal  @close='toggleModal'>
+        <h1 class="modalTitle">This is a slot</h1>
+        <p class="modalText">Yippie</p>
+        <template v-slot:links>
+          <a href="#">Sign Up!</a>
+          <a href="#">More Info</a>
+        </template>
+      </Modal>
+    </teleport>
+
+    <teleport to=".modals" v-if="showModalTwo">
+      <!-- Inne i Modal så är det något som kallas ett slot, vi fixar template som props ungefär -->
+      <Modal  @close='toggleModalTwo'>
+        <h1 class="modalTitle">This is the second Modal</h1>
+        <p class="modalText">Whohooo</p>
+        
+      </Modal>
+    </teleport>
+    <p>Welcome...</p>
+    <button @click="toggleModal">Show Modal</button>
+    <button @click="toggleModalTwo">Show Modal Two</button>
+<!-- </div> -->
 </template>
 
 <script>
+import Modal from './components/Modal'
 
 //Det här är 'root object/component', den har data, methods osv.
 export default {
   name: 'App',
+  components: {
+    Modal
+  },
   data() {
     return {
       title: 'My First Vue App',
-      desc: 'Learning Vue :)'
+      showModal: false,
+      showModalTwo: false,
     }
   },
   //Methods sköter metoder/functioner i våra components
@@ -28,6 +55,12 @@ export default {
       console.log(this.$refs.name)
       this.$refs.name.classList.add('active')
       this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   } 
 }
@@ -35,7 +68,7 @@ export default {
 
 /* Här är style, precis som css */
 <style>
-#wrapper {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -44,12 +77,20 @@ export default {
   box-sizing: border-box;
 }
 
-#wrapper > h1{
+#app > h1{
   border-bottom: 2px solid #ddd;
   padding-bottom: 10px;
 }
 
 #desc{
   font-size: 1.5rem;
+}
+
+.modalTitle {
+  color: white;
+}
+
+.modalText {
+  color: white;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+<!-- @click.self är ett event modifier, den säger att vi enbart kör functionen när vi i detta fall klickar på backdrop. -->
+    <div class="backdrop" @click.self="closeModal">
+        <!-- Här så använder vi 'slot' som vi bögg i App.vue -->
+        <div class="modal">
+            <slot />
+            <div class="actions">
+                <slot name='links'></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        
+    ],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+//Scoped betyder att dessa styles bara går på denna component
+<style scoped>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background-color: red;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background-color: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+  
+
+</style>
